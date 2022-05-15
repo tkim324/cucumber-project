1c8e8a2da7d5bfddd0240bd550b65d7f633020b5
S2juae <s2juae@gmail.com>
2022-05-15 19:02:37 +0200

    Homework-3rd

--- features/homework-third.feature
+++ features/homework-third.feature
@@ -1,18 +1,28 @@
-Feature: The Homework3rd : Search with Amazon
+Feature: Search with selenium-automation practice
 
 
   Background: The Browser should be opened
-    Given I open the Browser with url 'https://www.amazon.de'
+    Given I open the Browser with url 'http://automationpractice.com'
 
   Scenario: Search with screen UI
-    When I search 'geldbörse klein' in the input-box '#twotabsearchtextbox'
-    Then This attribute 'div.a-section #B07MY7Q5B3-best-seller' should be shown
-    When I click on the 'div.a-section #B07MY7Q5B3-best-seller' element
-    Then This attribute 'div._cDEzb_card-title_2sYgw h1.a-spacing-medium' should be shown
-    Then The result of attribute 'div.p13n-gridRow .a-column' is more then 10
-    Then The browser is closed
+  Then This attribute '#header_logo' should be shown
+  When I click on the '.login' element
+  Then This attribute '#SubmitCreate' should be shown
+  When I click on the '#SubmitCreate' element
+  When I wait element about 1000ms
+  Then This attribute '//*[@id="create_account_error"]/ol/li/text()' should be with 'Invalid email address'
+  Then The browser is closed
+  #   When I search 'geldbörse klein' in the input-box '#twotabsearchtextbox'
+  #   Then This attribute 'h2.a-size-mini span' should be shown with 'geld, börse, beutel'
+  #   Then This attribute 'div.a-section #B07MY7Q5B3-best-seller' should be shown
+  #  When I click on the 'div.a-section #B07MY7Q5B3-best-seller' element
+  #  Then This attribute 'div._cDEzb_card-title_2sYgw h1.a-spacing-medium' should be shown
+  #  Then The result of attribute 'div.p13n-gridRow .a-column' is more then 10
+  #  Then The browser is closed
 
   #찾아보고 대략적인 구분이 체크
-  # a, h1, h2
+  # a, h1, h2, ol, il
   # mark, span, aria-label
-
+  #원하는 문구를 찾기위한거라면 css로 찾는 것이 가능하나 (존재여부확인)
+  #원하는 문구의 정확한 위치를 찾으려면 xpath가 필요하다
+  #pending = 비동기적 처리
--- features/step_definitions/homework-third-step-def.js
+++ features/step_definitions/homework-third-step-def.js
@@ -4,31 +4,75 @@ const {Given, When, Then} = require('@cucumber/cucumber');
 const {expect} = require('chai');
 require('chromedriver')
 
-//1. 아마존 열기
+//4-1. 셀레니움 자동화시스템 연습용 사이트 열기
+Given(/^I open the Browser with url '(.*)'$/, async function (link) {
+    this.driver = new Builder().forBrowser('chrome').build();
+    await this.driver.get(link);
+    //await this.driver.findElement(By.id("sp-cc-accept")).click(); 쿠키설정
+});
+
+//4-2. 메인 로고 확인
+Then(/^This attribute '(.*)' should be shown$/, async function (cssValue) {
+    let isDisplayed = await this.driver.findElement(By.css(cssValue)).isDisplayed();
+});
+
+//4-3. 로그인 페이지로 이동
+When(/^I click on the '(.*)' element$/, async function (cssValue) {
+    await this.driver.findElement(By.css(cssValue)).click();
+});
+
+When(/^I wait element about 1000ms$/, async function () {
+    await this.driver.sleep(2000);
+    // await this.driver.wait(until.elementLocated(By.css(cssValue)),10000);
+});
+
+//4-4. 계정 생성 클릭
+//4-5. 에러메세지 확인
+Then(/^This attribute '(.*)' should be with 'Invalid email address'$/, async function (cssValue) {
+    let text = await this.driver.findElement(By.xpath(cssValue)).getText();
+    return expect(text).to.equal("Invalid email address");
+});
+
+Then (/^The browser is closed$/, async function(){
+    await this.driver.quit();
+});
+
+/*
+//3-1. 아마존 열기
 Given(/^I open the Browser with url '(.*)'$/, async function (link) {
     this.driver = new Builder().forBrowser('chrome').build();
     await this.driver.get(link);
     await this.driver.findElement(By.id("sp-cc-accept")).click();
 });
 
-//2. 아마존에서 검색하기
+//3-2. 아마존에서 검색하기
 When(/^I search '(.*)' in the input-box '(.*)'$/, async function (value, cssValue) {
     await this.driver.findElement(By.css(cssValue)).sendKeys(value, Key.RETURN);
 });
 
+//3-3.
+When(/^This attribute '(.*)' should be shown with 'geld, börse, beutel'$/, async function (cssValue) {
+    let elements = await this.driver.findElements(By.css(cssValue));
 
-//4. 검색결과 내 베스트셀러가 표기되어 있는지 확인
+    await Promise.all(elements.map(async elem =>{
+        const text = await elem.getText();
+        const keywords = ['geld', 'börse', 'beutel']
+        return expect(text.toLowerCase()).to.contains(keywords );
+    //    console.log(await elem.getText());
+    }));
+});
+//3-4. 검색결과 내 베스트셀러가 표기되어 있는지 확인
 Then(/^This attribute '(.*)' should be shown$/, async function (cssValue) {
     let isDisplayed = await this.driver.findElement(By.css(cssValue)).isDisplayed();
     return expect(isDisplayed).to.equal(true);
  });
 
-//5. 베스트셀러 링크 클릭
+//3-5. 베스트셀러 링크 클릭
 When(/^I click on the '(.*)' element$/, async function (cssValue) {
     await this.driver.findElement(By.css(cssValue)).click();
 });
 
-//6. 베스트셀러 내 검색결과 갯수 확인
+//3-6. 베스트셀러 내 검색결과 갯수 확인
 Then(/^The result of attribute '(.*)' is more then 10$/, async function (cssValue) {
      let count = (await this.driver.findElements(By.css(cssValue))).length;
      return expect(Number(count)).to.above(10);
@@ -37,3 +81,6 @@ Then(/^The result of attribute '(.*)' is more then 10$/, async function (cssValu
 Then (/^The browser is closed$/, async function(){
     await this.driver.quit();
 });
+*/
+
+
